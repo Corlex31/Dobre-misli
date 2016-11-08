e23b855f1252e45f1ebd74e52f3b72ca04a989f3
Corlex31 <killerworm31@gmail.com>
2016-11-08 10:38:10 +0100

    rešitev

--- Dobre misli/Default.aspx.cs
+++ Dobre misli/Default.aspx.cs
@@ -11,12 +11,26 @@ namespace Dobre_misli
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            
+            if (Application["misli"] == null)
+            {
+                Application["misli"] = new List<string>();
+            }
+
         }
 
         protected void dodajMisel_Click(object sender, EventArgs e)
         {
-                    
+            var misli = (List<string>)Application["Misli"];
+            misli.Insert(0, misel.Text);
+          
+
+            foreach (var text in misli)
+            {
+                var label = new Label();
+                label.Text = "\"" + text + "\"" + new HtmlString("<br />");
+                FindControl("misli").Controls.Add(label);
+            }
+
         }
     }
     
